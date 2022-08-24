729d684618c3e2c31862094dd035cdaccc75b7f9
Ajay Singh <xpadajay@gmail.com>
2022-08-24 15:11:27 +0530

    js added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 $(document).ready(function(){
-    
+    $('.calculator-button-main-block button').on('click',function(){
+        let a = $(this).val();
+        $('.calculator-display-block h2').html(a);
+    });
 });
