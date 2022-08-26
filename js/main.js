642263390fce32311938f1f460508068de62c914
Ajay Singh <xpadajay@gmail.com>
2022-08-26 14:18:59 +0530

    functionality added

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
 $(document).ready(function(){
     $('.calculator-button-main-block button').on('click',function(){
-        let a = $(this).val();
-        $('.calculator-display-block h2').html(a);
+        let btnValue = $(this).val();
+        $('.calculator-display-block input').val($('.calculator-display-block input').val() + btnValue);
+    });
+    $('.clear-screen-btn').on('click',function(){
+        $('.calculator-display-block input').val('0');
+    });
+    let digitCount = $('.calculator-display-block input').val();
+    if(digitCount.length > '4'){
+        $('.calculator-display-block input').css('font-size','2.6rem');
+        console.log(digitCount.length);
+    }
+    if($('.display-screen').length != '0'){
+        $('.clear-screen-btn').html('C');
+    }
+    //Addition Function//
+    $('.result-btn').on('click',function(){
+        let result = $('.display-screen').val();
+        eval(result);
+        $('.display-screen').val(result);
     });
 });
